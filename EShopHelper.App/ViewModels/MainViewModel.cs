63a0f0796d5a584903baa2f6f7e4270e8d75df19
HuangShuhang <yhnbgfd@gmail.com>
2024-04-16 17:58:44 +0800

    del Avalonia

--- EShopHelper.App/ViewModels/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace EShopHelper.App.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- EShopHelper.App/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace EShopHelper.App.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
