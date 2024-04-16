dc715fb14d17b7c42d126ed1dd04eb75ed5c11b8
HuangShuhang <yhnbgfd@gmail.com>
2024-04-16 15:09:55 +0800

    +Avalonia

---
+++ EShopHelper.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace EShopHelper.App.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ EShopHelper.App/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace EShopHelper.App.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
