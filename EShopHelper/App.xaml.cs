9533538aca4dd0003b4577bb80fbdf0b2e46d7fb
None <None@None.com>
2024-05-02 00:43:31 +0800

    +全局异常处理

--- EShopHelper/App.xaml.cs
+++ EShopHelper/App.xaml.cs
@@ -1,11 +1,23 @@
 ﻿using NLog;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace EShopHelper
 {
     public partial class App : Application
     {
         private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public App()
+        {
+            //在异常由应用程序引发但未进行处理时发生。UI线程
+            //无法捕获多线程异常
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            //专门捕获所有线程中的异常
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            //专门捕获Task异常
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+        }
 
         private async void Application_Startup(object sender, StartupEventArgs e)
         {
@@ -40,6 +52,24 @@ namespace EShopHelper
                     Environment.Exit(0);
                 }
             }
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), "DispatcherUnhandledException");
+            Environment.Exit(1);
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString(), "UnhandledException");
+            Environment.Exit(1);
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception)?.ToString(), "UnobservedTaskException");
+            Environment.Exit(1);
         }
     }
 }
