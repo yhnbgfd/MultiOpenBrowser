fef5230d407e96eee80df7211d230a5e6be39aea
None <None@None.com>
2024-05-02 04:25:38 +0800

    =

--- EShopHelper/Entitys/Option.cs
+++ EShopHelper/Entitys/Option.cs
@@ -10,6 +10,9 @@ namespace EShopHelper.Entitys
         /// 默认浏览器数据文件夹
         /// </summary>
         public string DefaultWebBrowserDataPath { get; set; } = "";
+        /// <summary>
+        /// 默认 User-Agent
+        /// </summary>
         public string DefaultUserAgent { get; set; } = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36 NGA_WP_JW/(;WINDOWS)";
     }
 }
