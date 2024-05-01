47378af042cb9126a2e04018ef05943340c83a45
None <None@None.com>
2024-05-01 23:11:27 +0800

    delete TestWin

--- EShopHelper/App.xaml.cs
+++ EShopHelper/App.xaml.cs
@@ -7,7 +7,6 @@ namespace EShopHelper
         private void Application_Startup(object sender, StartupEventArgs e)
         {
             StartupUri = new Uri("Views/Windows/MainWindow.xaml", UriKind.Relative);
-            //StartupUri = new Uri("Views/Windows/TestWindow.xaml", UriKind.Relative);
         }
     }
 
