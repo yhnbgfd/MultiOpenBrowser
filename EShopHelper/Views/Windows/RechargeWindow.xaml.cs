7562f358bb84f10ab7ef937489dc9c24807c7a77
None <None@None.com>
2024-05-03 02:43:54 +0800

    +RechargeWindow

---
+++ EShopHelper/Views/Windows/RechargeWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace EShopHelper.Views.Windows
+{
+    public partial class RechargeWindow : Window
+    {
+        public RechargeWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void CloseCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
