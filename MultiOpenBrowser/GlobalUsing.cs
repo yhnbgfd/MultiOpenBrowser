44098bc36e96aafa218c3c692da339a11b25444f
HuangShuhang <yhnbgfd@gmail.com>
2024-09-26 14:21:30 +0800

    更换 gloabl using 写法

--- MultiOpenBrowser/GlobalUsing.cs
+++
@@ -1,9 +0,0 @@
-﻿global using FreeSql;
-global using FreeSql.DataAnnotations;
-global using MultiOpenBrowser.Base;
-global using MultiOpenBrowser.Base.Generic;
-global using MultiOpenBrowser.Entitys;
-global using MultiOpenBrowser.Helpers;
-global using MultiOpenBrowser.Repositorys;
-global using NLog;
-global using System.ComponentModel;
