cbb388d5f992f72f00b51999ea60a3832b59f7bd
HuangShuhang <yhnbgfd@gmail.com>
2024-11-05 15:18:50 +0800

    init Avalonia project

---
+++ MultiBrowserEnvTool/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace MultiBrowserEnvTool.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MultiBrowserEnvTool/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace MultiBrowserEnvTool.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
