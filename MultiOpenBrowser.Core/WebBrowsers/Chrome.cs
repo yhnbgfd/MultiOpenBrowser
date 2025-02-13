e009538643497a1070597d74b52984c8295c786f
yhnbgfd <yhnbgfd@gmail.com>
2025-02-13 09:57:05 +0800

    取消: hide-crash-restore-bubble

--- MultiOpenBrowser.Core/WebBrowsers/Chrome.cs
+++ MultiOpenBrowser.Core/WebBrowsers/Chrome.cs
@@ -30,7 +30,7 @@ namespace MultiOpenBrowser.Core.WebBrowsers
             {
                 AppendArgument(sb, "restore-last-session");
             }
-            AppendArgument(sb, "hide-crash-restore-bubble");
+            //AppendArgument(sb, "hide-crash-restore-bubble");
 
             if (_webEnvironment.WebBrowser.DisableWebSecurity)
             {
