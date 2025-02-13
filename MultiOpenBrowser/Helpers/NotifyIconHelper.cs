54350e2d0f6a388a1e89d08ffae4ecdfa0031cdb
yhnbgfd <yhnbgfd@gmail.com>
2025-02-13 18:49:45 +0800

    8

---
+++ MultiOpenBrowser/Helpers/NotifyIconHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiOpenBrowser.Helpers
+{
+    internal class NotifyIconHelper
+    {
+        //private NotifyIcon notifyIcon = new();
+    }
+}
